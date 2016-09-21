52554dfa7546cdba4133cfa980846b1be62c4811
YevgeniyShunevych <eshunevich@gmail.com>
2016-09-21 11:56:51 +0300

    Update sidebar

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,6 @@
     'use strict';
 
     $(function () {
-
         // Scrollspy
         var $window = $(window)
         var $body = $(document.body)
@@ -14,29 +13,8 @@
             $body.scrollspy('refresh')
         })
 
-        // Kill links
         $('.at-docs-container [href="#"]').click(function (e) {
             e.preventDefault()
         })
-
-        // Sidenav affixing
-        setTimeout(function () {
-            var $sideBar = $('.at-docs-sidebar')
-
-            $sideBar.affix({
-                offset: {
-                    top: function () {
-                        var offsetTop = $sideBar.offset().top
-                        var sideBarMargin = parseInt($sideBar.children(0).css('margin-top'), 10)
-                        var navOuterHeight = $('.at-docs-nav').height()
-
-                        return (this.top = offsetTop - navOuterHeight - sideBarMargin)
-                    },
-                    bottom: function () {
-                        return (this.bottom = $('.at-docs-footer').outerHeight(true))
-                    }
-                }
-            })
-        }, 100)
     })
 }(jQuery);
